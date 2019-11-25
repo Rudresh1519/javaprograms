9e9dfe1e603d9b8306d4c0233fe575fa10c1dd15
Rudresh1519 <58024880+Rudresh1519@users.noreply.github.com>
2019-11-25 10:04:16 +0530

    Add files via upload

---
+++ ThreadTest.java
@@ -0,0 +1,86 @@
+package assignment_2a;
+
+public class ThreadTest {
+
+	private static int count = 0;
+	
+	static String[] string1 = { "", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen","fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "ninteen"};
+	 static String[] string2 = { "", "", "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninty","hundred"};
+	
+	 public static void check(int number1,String string3)
+		{
+		  if(number1>19)
+		  {
+			  System.out.print(string2[number1/10] +""+ string1[number1%10]);
+		  }
+		  else
+			  System.out.print(string1[number1]);
+		  
+		  if(number1>0)
+			  System.out.print(" "+string3+" ");
+		  
+		}
+	 
+	public static void main(String[] args) throws InterruptedException {
+		
+		 Thread t1 = new Thread(new Runnable() {
+
+		        @Override
+		        public void run() {
+
+		            for (int i = 1; i <100; i++) {
+		                synchronized (sampleClass.tObject) {
+		                    incrementCount();
+		                    sampleClass.tObject.notify();
+		                    try {
+		                        sampleClass.tObject.wait();
+		                    } catch (InterruptedException e) {
+		                        // TODO Auto-generated catch block
+		                        e.printStackTrace();
+		                    }
+		                }
+		            }
+		            
+		        }
+		    });
+		    Thread t2 = new Thread(new Runnable() {
+
+		        @Override
+		        public void run() {
+
+		            for (int i = 2; i <100; i++) {
+		                synchronized (sampleClass.tObject) {
+		                    incrementCount();
+		                    sampleClass.tObject.notify();
+		                    try {
+		                        sampleClass.tObject.wait();
+		                    } catch (InterruptedException e) {
+		                        // TODO Auto-generated catch block
+		                        e.printStackTrace();
+		                    }
+		                }  
+		            }
+		            System.out.println("hundred");
+		        }
+		    });
+		    t1.start();
+		    Thread.sleep(400);
+		    t2.start();
+		    t1.join();
+		    t2.join();
+		}
+
+		private static void incrementCount() {
+
+			check(count++," ");
+		   }
+		}
+
+		  class sampleClass {
+
+		 static final Object tObject = new Object();
+		   
+
+	}
+
+
